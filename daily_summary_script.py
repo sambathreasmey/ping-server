21e9c88821db32fec15448a7de9a04007e3d50ed
19134 <reasmeysambath@gamil.com>
2026-02-11 13:41:07 +0700

    test

--- daily_summary_script.py
+++ daily_summary_script.py
@@ -1,7 +1,12 @@
 import time
+import os
+import requests
+
+from generate import create_card_v2
 
 # --- CONFIG ---
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+SEND_CHAT_ID = os.getenv("SEND_CHAT_ID")
 
 def main():
     try:
@@ -10,6 +15,35 @@ def main():
         print(f"Error: {e}")
 
 def push_telegram():
+    issueName = "SMEY"
+    changeUpDown = "up"
+    currentPrice = "18,000"
+    percentChange = "10.25"
+    change = "1,000"
+    issueSummary = []
+    
+    img_path = create_card_v2(issueName, changeUpDown, currentPrice, f"{percentChange}%", change, issueSummary)
+    up_down_equal = ""
+    # Send to Telegram
+    if changeUpDown == "up":
+        up_down_equal = "🟢ឡើង"
+    elif changeUpDown == "down":
+        up_down_equal = "🔴ចុះ"
+    else:
+        up_down_equal = "⚫️ស្មើរ"
+    caption = f"<b>{issueName} {currentPrice} រៀល</b> {up_down_equal} {change} | <b>{percentChange}%</b>"
+    try:
+        with open(img_path, "rb") as img:
+            response = requests.post(
+                f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto", 
+                data={"chat_id": SEND_CHAT_ID, "caption": caption, "parse_mode": "HTML"},
+                files={"photo": img}
+            )
+            print(f"Telegram response: {response.status_code}")
+    finally:
+        if os.path.exists(img_path):
+            os.remove(img_path)
+            print(f"🗑️ Deleted local file: {img_path}")
     time.sleep(20)
 
 if __name__ == "__main__":
--- generate.py
+++ generate.py
@@ -74,3 +74,79 @@ def create_card(symbol, status, value, percent, change, issueSummary):
     print(f"✔ Saved {filename}")
 
     return filename
+
+def create_card_v2(symbol, status, value, percent, change, issueSummary):
+    # Colors for each card type
+    colors = {
+        "UP":    "#4AFF47",  # green
+        "DOWN":  "#FF0000",  # red
+        "EQUAL": "#FFFFFF",  # gray
+    }
+
+    chagne_symbols = {
+        "UP":    "+",  # green
+        "DOWN":  "-",  # red
+        "EQUAL": "",  # gray
+    }
+
+    color = colors.get(status.upper(), "#000000")
+    chagne_symbol = chagne_symbols.get(status.upper(), "")
+
+    # 2× scale factor
+    SCALE = 3
+
+    # Card size
+    W, H = 280 * SCALE, 125 * SCALE
+
+
+    # img = Image.new("RGB", (W, H), "white")
+    # Load background image
+    if status.upper() == "UP-SKIP":
+        bg_img = Image.open(f"images/background-down-2.jpg")
+    else:
+        bg_img = Image.open(f"images/background-{status.lower()}.png")
+    bg_img = bg_img.resize((W, H))         # resize to card size
+
+    # Create card image based on background
+    img = bg_img.convert("RGB")             # ensure RGB mode
+
+
+    draw = ImageDraw.Draw(img)
+
+    # Rounded left border (scaled)
+    draw.rounded_rectangle([-16, 0, 36, H], radius=20, fill=color)
+
+    # Load Bold Fonts (scaled)
+    try:
+        font_main = ImageFont.truetype("fonts/DejaVuSans-Bold.ttf", 34 * SCALE)
+        font_small = ImageFont.truetype("fonts/DejaVuSans-Bold.ttf", 26 * SCALE)
+        font_smallest = ImageFont.truetype("fonts/DejaVuSans-Bold.ttf", 6 * SCALE)
+    except Exception as e:
+        print("Font loading error:", e)
+        font_main = ImageFont.load_default()
+        font_main = ImageFont.load_default()
+
+    # Draw text (scaled positions)
+    draw.text((35 * SCALE, 15 * SCALE), symbol, fill=color, font=font_main)
+    draw.text((35 * SCALE, 55 * SCALE), value, fill=color, font=font_main)
+    draw.text((35 * SCALE, 90 * SCALE), percent, fill=color, font=font_small)
+    draw.text((127 * SCALE, 108 * SCALE), issueSummary, fill=colors.get("#FFFFFF"), font=font_smallest)
+
+    text = chagne_symbol + str(change)
+    right_x = 260 * SCALE
+    y = 78 * SCALE
+
+    # Option 1: Using textbbox (new Pillow)
+    bbox = draw.textbbox((0, 0), text, font=font_small)
+    text_width = bbox[2] - bbox[0]
+
+    x = right_x - text_width
+    draw.text((x, y), text, fill=color, font=font_small)
+
+    # Save
+    filename = f"images/output_v2.png"
+    img.save(filename)
+    print(f"✔ Saved {filename}")
+
+    return filename
+
